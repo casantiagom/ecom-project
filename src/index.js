64d821d0c34cbe349c0fe17bee6fee0758a51af6
casantiagom <66339441+casantiagom@users.noreply.github.com>
2020-07-30 18:33:01 -0400

    added redux, impemented userReducer

--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-
+import {Provider} from 'react-redux';
+import store from './redux/store';
 import {BrowserRouter} from 'react-router-dom';
 import App from './App';
 
 ReactDOM.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>,
   document.getElementById('root')
 );
